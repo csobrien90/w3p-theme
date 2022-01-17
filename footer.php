4c2f09ca6326e0849ed08b195527ca2ffe4f513a
Chad OBrien <obrien.music@gmail.com>
2022-01-17 14:35:24 -0500

    remove footer widgets

--- footer.php
+++ footer.php
@@ -4,19 +4,12 @@
  *
  * Contains the closing of the #content div and all content after.
  *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
  */
 
 ?>
 			</main><!-- #main -->
 		</div><!-- #primary -->
 	</div><!-- #content -->
-
-	<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 
@@ -53,16 +46,6 @@
 					<?php endif; ?>
 				<?php endif; ?>
 			</div><!-- .site-name -->
-			<div class="powered-by">
-				<?php
-				printf(
-					/* translators: %s: WordPress. */
-					esc_html__( 'Proudly powered by %s.', 'twentytwentyone' ),
-					'<a href="' . esc_url( __( 'https://wordpress.org/', 'twentytwentyone' ) ) . '">WordPress</a>'
-				);
-				?>
-			</div><!-- .powered-by -->
-
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 
